408219099b4618eebbf9f5641a7714a791bfd12c
nicolaspieper59-ops <nicolas.pieper59@gmail.com>
2025-09-21 09:53:50 +0000

    Update Config.js

--- modules/Config.js
+++ modules/Config.js
@@ -1,19 +1,6 @@
-export const modules = {
-  acceleration: "modules/acceleration.html",
-  orientation: "modules/orientation.html",
-  energie: "modules/energie.html",
-  temperature: "modules/temperature.html",
-  humidite: "modules/humidite.html",
-  distance: "modules/distance.html",
-  gps: "modules/gps-precision.html",
-  rtx: "modules/rtx-vitesse.html",
-  inertiel: "modules/inertiel.html",
-  solaire: "modules/solaire-gradient.html",
-  lunaire: "modules/lunaire-phase.html",
-  accelG: "modules/acceleration-g.html",
-  force: "modules/force-newton.html",
-  relatif: "modules/temps-relatif.html",
-  historique: "modules/historique.html",
-  intro: "modules/intro-cosmique.html",
-  mobile: "modules/mobile-panel.html"
+export const config = {
+  theme: "cosmic",
+  ambiance: "galactique",
+  precision: "haute",
+  modulesActifs: []
 };
